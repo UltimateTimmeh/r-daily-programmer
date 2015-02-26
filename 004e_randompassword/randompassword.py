fc6f544d0d5223efbf1c8f22ba72757534e3082a
UltimateTimmeh <UltimateTimmeh@users.noreply.github.com>
2015-02-26 15:09:07 +0100

    Added first version of 004e_randompassword

---
+++ 004e_randompassword/randompassword.py
@@ -0,0 +1,36 @@
+"""
+Generate p random passwords with length l. Includes alphanumerical characters
+and some symbols.
+
+To use in a terminal:
+
+$ python3.x randompassword.py p l
+
+Where p and l are integers.
+"""
+
+import random
+import sys
+
+
+def generate_passwords(p, l):
+    """Generate p random password consisting of l characters."""
+    symbols = "abcdefghijklmnopqrstuvwxyz0123456789<>?!@#$%^&*()_+-=[];{}:"
+    symbols += "ABCDEFGHILJKLMNOPQRSTUVWXYZ"
+    passwords = []
+    for ii in range(p):
+        password = [symbols[random.randint(0,len(symbols)-1)] \
+                    for jj in range(l)]
+        password = ''.join(password)
+        passwords.append(password)
+    return passwords
+
+
+if __name__ == '__main__':
+    # Get the command line options.
+    p = int(sys.argv[1])
+    l = int(sys.argv[2])
+    for password in generate_passwords(p, l):
+        print(password)
+
+# End
