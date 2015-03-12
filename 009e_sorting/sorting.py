feed606ecf9a3abdfac654f01325d8d6cae4fa9d
UltimateTimmeh <UltimateTimmeh@users.noreply.github.com>
2015-03-12 16:32:09 +0100

    009e_sorting: Add/optimize algorithms, add algorithm timing
    Insertion sort was optimized to make it quite a bit faster. Merge sort was
    added. The main section of the script now feeds a shuffled list to the
    different sorting algorithms, keeps track of the run time for each and
    prints a report.

--- 009e_sorting/sorting.py
+++ 009e_sorting/sorting.py
@@ -4,6 +4,7 @@ Implementation of various sorting algorithms, and tests on
 various list sizes to determine which ones are more efficient.
 """
 
+import datetime
 import random
 
 
@@ -13,17 +14,23 @@ def sort_insertion(_list):
     Algorithm:
     1) Go over all items of the list, starting at the second item (the first
        one is automatically in the correct position). The current item will be
-       called the pivot.
-    2) While the pivot is not at the start of the list and the item before the
-       pivot is larger than the pivot, swap the pivot with the item before it.
+       called the pivot, and is taken out of the list (consider its original
+       space to now be empty).
+    2) Start moving to the left of the pivot. Shift the encountered items one
+       space to right (effectively moving the empty space to the left), but
+       stop once you encounter an item that is smaller than the pivot or once
+       shifting is no longer possible (i.e. the empty space reaches the start
+       of the list).
+    3) Place the pivot into the resulting empty space.
+    4) After going over all elements in the list, the list is sorted.
     """
-    for pivot in range(1, len(_list)):
-        jj = pivot
-        while jj > 0 and _list[jj-1] > _list[jj]:
-            backup = _list[jj]
-            _list[jj] = _list[jj-1]
-            _list[jj-1] = backup
-            jj -= 1
+    for ip in range(1, len(_list)):
+        pivot = _list[ip]
+        il = ip
+        while il > 0 and _list[il-1] > pivot:
+            _list[il] = _list[il-1]
+            il -= 1
+        _list[il] = pivot
     return _list
 
 
@@ -37,24 +44,81 @@ def sort_selection(_list):
        and keep track of the smallest element.
     3) Once the end of the list has been reached, swap the pivot with the
        smallest element.
+    4) After going over all elements in the list, the list is sorted.
     """
-    for pivot in range(len(_list)):
-        smallest = pivot
-        for jj in range(pivot+1,  len(_list)):
-            if _list[jj] < _list[smallest]:
-                smallest = jj
-        backup = _list[pivot]
-        _list[pivot] = _list[smallest]
-        _list[smallest] = backup
+    for ip in range(len(_list)):
+        smallest = ip
+        for ir in range(ip+1,  len(_list)):
+            if _list[ir] < _list[smallest]:
+                smallest = ir
+        if ip != smallest:
+            pivot = _list[ip]
+            _list[ip] = _list[smallest]
+            _list[smallest] = pivot
     return _list
 
 
+def sort_merge(_list):
+    """Implementation of merge sort.
+
+    Algorithm:
+    1) Keep dividing the list in half until you reach the smallest possible size.
+    2) Repeatedly merge the lists back together in pairs, meanwhile sorting the
+       elements.
+    3) Once all parts have been merged back together, the list is sorted.
+    """
+    ll = len(_list)
+    if ll <= 1:
+        return _list
+    left = sort_merge(_list[:int(ll/2)])
+    right = sort_merge(_list[int(ll/2):])
+    merged = []
+    il = 0
+    ir = 0
+    while il != len(left) and ir != len(right):
+        if left[il] < right[ir]:
+            merged.append(left[il])
+            il += 1
+        else:
+            merged.append(right[ir])
+            ir += 1
+    return merged + left[il:] + right[ir:]
+
+
 if __name__ == '__main__':
-    nelems = 10
+    # Create a list containing a chosen amount of numbers, and randomize.
+    nelems = 10000
     elems = list(range(nelems))
     random.shuffle(elems)
-    print(elems)
-    elems_sorted = sort_selection(elems)
-    print(elems_sorted)
+
+    # The sorting algorithms to be considered.
+    sort_names = [
+        'Insertion sort',
+        'Selection sort',
+        'Merge sort',
+        ]
+    sort_funcs = [
+        sort_insertion,
+        sort_selection,
+        sort_merge,
+        ]
+
+    # Feed the list to the sorting algorithms, time the execution and print a report.
+    for sort_name,  sort_func in zip(sort_names, sort_funcs):
+        elems_copy = [e for e in elems]
+        ts = datetime.datetime.now()
+        elems_sorted = sort_func(elems_copy)
+        te = datetime.datetime.now()
+        td = te - ts
+        txt = [
+            '\n'.join(['='*len(sort_name),  sort_name,  '='*len(sort_name)]),
+            ## "Original: {0}",
+            ## "Sorted: {1}",
+            "Amount of elements: {2}",
+            "Elapsed time: {3}",
+            ]
+        txt = '\n'.join(txt)
+        print('\n'+txt.format(elems, elems_sorted, len(elems), td.total_seconds()))
+
 
 # End
