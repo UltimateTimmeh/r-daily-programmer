4ce047529d3016614f28f931095c64ba2713d22e
UltimateTimmeh <UltimateTimmeh@users.noreply.github.com>
2015-04-09 14:50:48 +0200

    Minor restructuring of tests for plugins.personalinfo

--- dailyprogrammer/tests/test_personalinfo.py
+++ dailyprogrammer/tests/test_personalinfo.py
@@ -8,7 +8,7 @@ import shutil
 import unittest
 
 import plugins.config as cfg
-from plugins.personalinfo import PersonalInfo
+from plugins import personalinfo
 
 
 class TestPersonalInfo(unittest.TestCase):
@@ -16,52 +16,66 @@ class TestPersonalInfo(unittest.TestCase):
 
     def setUp(self):
         """Set up before test"""
-        self.pi = PersonalInfo('John Smith', 50, reddit_username='John_Smith')
-        self.pi_str_expected = [
+        os.makedirs(cfg.tmp_dir, exist_ok=True)
+
+
+    def tearDown(self):
+        """Tear down after test"""
+        if os.path.isdir(cfg.tmp_dir):
+            shutil.rmtree(cfg.tmp_dir)
+
+
+    def test___init__(self):
+        """Test method personalinfo.PersonalInfo.__init__"""
+        pi = personalinfo.PersonalInfo(
+            'John Smith',
+            50,
+            reddit_username='John_Smith',
+            personal_attribute='personal_value',
+            )
+        self.assertEqual(pi.name, 'John Smith')
+        self.assertEqual(pi.age, 50)
+        self.assertEqual(pi.reddit_username, 'John_Smith')
+        self.assertEqual(pi.personal_attribute, 'personal_value')
+
+
+    def test___str__(self):
+        """Test method personalinfo.PersonalInfo.__str__"""
+        pi = personalinfo.PersonalInfo(
+            'John Smith',
+            50,
+            reddit_username='John_Smith',
+            personal_attribute='personal_value',
+            )
+        result = pi.__str__()
+        expected = [
             "Personal information of John Smith (50 years old):",
             "name: John Smith",
             "age: 50",
             "reddit_username: John_Smith",
             ]
-        os.makedirs(cfg.tmp_dir, exist_ok=True)
-        self.testfile_fn = os.path.join(cfg.tmp_dir, 'testoutput.txt')
-
-
-    def tearDown(self):
-        """Tear down after test"""
-        # Delete test files.
-        if os.path.isdir(cfg.tmp_dir):
-            shutil.rmtree(cfg.tmp_dir)
-
-        # Delete test attributes.
-        attr_to_del = [
-            'pi',
-            'pi_str_expected',
-            'testfile_fn',
-            ]
-        for attr in attr_to_del:
-            delattr(self, attr)
-
-
-    def test___init__(self):
-        """Test method personalinfo.PersonalInfo.__init__"""
-        self.assertEqual(self.pi.name, 'John Smith')
-        self.assertEqual(self.pi.age, 50)
-        self.assertEqual(self.pi.reddit_username, 'John_Smith')
-
-
-    def test___str__(self):
-        """Test method personalinfo.PersonalInfo.__str__"""
-        received = self.pi.__str__()
-        self.assertTrue(all([expected in received for expected in self.pi_str_expected]))
+        self.assertTrue(all([part in result for part in expected]))
 
 
     def test_write(self):
         """Test method personalinfo.PersonalInfo.write"""
-        self.pi.write(self.testfile_fn)
-        with open(self.testfile_fn, 'r') as testfile:
-            received = testfile.read()
-        self.assertTrue(all([expected in received for expected in self.pi_str_expected]))
+        pi = personalinfo.PersonalInfo(
+            'John Smith',
+            50,
+            reddit_username='John_Smith',
+            personal_attribute='personal_value',
+            )
+        testfile_fn = os.path.join(cfg.tmp_dir, 'testoutput.txt')
+        pi.write(testfile_fn)
+        with open(testfile_fn, 'r') as testfile:
+            result = testfile.read()
+        expected = [
+            "Personal information of John Smith (50 years old):",
+            "name: John Smith",
+            "age: 50",
+            "reddit_username: John_Smith",
+            ]
+        self.assertTrue(all([part in result for part in expected]))
 
 
 if __name__ == '__main__':
