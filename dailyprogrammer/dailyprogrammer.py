75c751bfb36e8773308a792d719ed6294942866f
UltimateTimmeh <UltimateTimmeh@users.noreply.github.com>
2015-06-18 16:51:16 +0200

    Added method for automatically preparing a challenge solution module blueprint
    The main dailyprogrammer execution script was updated to include a
    way for automatically generating a challenge solution module blueprint.
    
    A command-line argument 'prepare' was added which, combined with a
    challenge ID, automatically finds the url to the correct submission
    on Reddit using BeatifulSoup and then extracts the challenge's
    information from the submission using PRAW. Finally, this information
    is used to format a challenge template file.
    
    Some safeties have been built in. Preparing a blueprint for a challenge
    is not possible if there is already a module with that challenge ID.
    If multiple challenges with the ID are found, a warning is printed and
    the first one is automatically picked for generation. The others will
    have to be created manually, with a non-default challenge ID.
    
    This change also meant that an update was required for how challenge
    solutions are executed. For this, a command-line argument 'execute'
    was added which, combined with a challenge ID, will execute that
    challenge (if it exists).

--- dailyprogrammer/dailyprogrammer.py
+++ dailyprogrammer/dailyprogrammer.py
@@ -1,5 +1,74 @@
+from bs4 import BeautifulSoup
+import datetime
 import importlib
+import os
+import praw
 import sys
-challenge_id = sys.argv[1]
-challenge = importlib.import_module('challenges.{}'.format(challenge_id))
-challenge.run()
+from urllib.request import urlopen
+
+
+def execute(challenge_id):
+    """Execute a challenge."""
+    if not os.path.isfile(os.path.join('challenges', '{}.py'.format(challenge_id))):
+        print("\nERROR: Could not find solution module for challenge {}!".format(challenge_id))
+        return
+    challenge = importlib.import_module('challenges.{}'.format(challenge_id))
+    challenge.run()
+
+
+def prepare(challenge_id):
+    """Prepare a challenge's solution module."""
+    # Make sure the file doesn't already exist. We don't want to risk overwriting!
+    challenge_path = os.path.join('challenges', '{}.py'.format(challenge_id))
+    if os.path.isfile(challenge_path):
+        print("\nERROR: Solution module for challenge {} already exists!".format(challenge_id))
+        return
+
+    # Split challenge id in number and difficulty.
+    challenge = {'id': challenge_id}
+    challenge['nr'] = int(challenge['id'][:-1])
+    challenge['difficulty'] = {'e': 'Easy', 'i': 'Intermediate', 'h': 'Hard'}[challenge['id'][-1]]
+    challenge_title = 'challenge #{} [{}]'.format(challenge['nr'], challenge['difficulty'].lower())
+
+    # Fetch the url of the challenge's post from Reddit.
+    url_base = 'http://www.reddit.com'
+    url = url_base + '/r/dailyprogrammer/wiki/challenges'
+    page = urlopen(url).read().decode('utf-8')
+    soup = BeautifulSoup(page)
+    url_match = [link.get('href') for link in soup.find_all('a') \
+        if link.string is not None and challenge_title in link.string.lower()]
+
+    if len(url_match) == 0:
+        print("\nERROR: No challenge with title '{}' was found.".format(challenge_title))
+        return
+    elif len(url_match) > 1:
+        print("\nWARNING: Multiple challenges with title '{}' were found:".format(challenge_title))
+        print('\n'.join(url_match))
+        print("Automatically picking the first one. Others will have to be prepared manually.")
+    challenge['url'] = url_base + url_match[0]
+
+    print("\nPreparing challenge {} from url:\n{}".format(challenge['id'], challenge['url']))
+
+    # Get challenge data from challenge post using PRAW
+    r = praw.Reddit(user_agent='dailyprogrammer')
+    challenge_submission = r.get_submission(challenge['url'])
+    challenge['description'] = challenge_submission.selftext
+    challenge['date'] = str(datetime.date.fromtimestamp(challenge_submission.created))
+
+    # Create the challenge module blueprint by formatting the template.
+    template_path = os.path.join('templates','challenge.py')
+    with open(template_path, 'r') as fil:
+        template = fil.read()
+    with open(challenge_path, 'w') as fil:
+        fil.write(template.format(**challenge))
+    print("DONE")
+
+
+if __name__ == '__main__':
+    actions = {
+        'execute': execute,
+        'prepare': prepare,
+    }
+    action,  challenge_id = sys.argv[1:]
+    actions[action](challenge_id)
+
---
+++ dailyprogrammer/templates/challenge.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Information
+-----------
+
+.. _reddit: {url}
+.. _source: https://github.com/UltimateTimmeh/r-daily-programmer/blob/master/dailyprogrammer/challenges/{id}.py
+
+| **Challenge name:**  (reddit_, source_)
+| **Challenge number:** {nr}
+| **Difficulty:** {difficulty}
+| **Submission date:** {date}
+| **Status:** In Progress
+
+Description
+-----------
+
+{description}
+
+Example run
+-----------
+
+::
+
+
+
+Module contents
+---------------
+"""
+
+
+def run():
+    """Execute the challenges.{id} module."""
+
